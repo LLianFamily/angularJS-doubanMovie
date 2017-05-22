f6b290c43571e42f4a5ebe6a09a36e7d6c6f7902
zzc5464 <774143745@qq.com>
2017-05-22 14:41:24 +0800

    '功能基本完成，还差个点击焦点的样式'

--- app/app.js
+++ app/app.js
@@ -2,11 +2,9 @@
 	//主要模块
 	var mainModule = angular.module('moviecat', [
 		'ngRoute',
-		'moviecat.in_theaters',
-		'moviecat.coming_soon',
-		'moviecat.top250'
+		'moviecat.movie_list'
 	]);
 	mainModule.config(['$routeProvider',function($routeProvider) {
-		$routeProvider.otherwise({redirectTo:'/in_theaters'})
+		$routeProvider.otherwise({redirectTo:'/in_theaters/1'})
 	}])
 })(angular)
--- app/movie_list/controller.js
+++ app/movie_list/controller.js
@@ -4,17 +4,17 @@
 	 *
 	 * Description
 	 */
-	var module = angular.module('moviecat.in_theaters', [
+	var module = angular.module('moviecat.movie_list', [
 		'ngRoute'
 		,'moviecat.server.http'
 		]);
 	module.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/in_theaters/:page', {
-			templateUrl: 'in_theaters/view.html',
-			controller: 'InTheatersContorller'
+		$routeProvider.when('/:category/:page', {
+			templateUrl: 'movie_list/view.html',
+			controller: 'MovieListContorller'
 		});
 	}]);
-	module.controller('InTheatersContorller', [
+	module.controller('MovieListContorller', [
 		'$scope'
 		,'$route'
 		,'$routeParams'
@@ -32,7 +32,8 @@
 			$scope.pageCount = 0;
 			$scope.loading = true;
 			$scope.currentPage = page;//当前页数
-			httpServer.jsonp('http://api.douban.com/v2/movie/in_theaters'
+			httpServer.jsonp(
+				'http://api.douban.com/v2/movie/'+$routeParams.category
 				,{
 					start:start,
 					count:count
@@ -54,35 +55,8 @@
 				*/
 				//合法化判断
 				if(page>=1&&page<=$scope.pageCount)
-				$route.updateParams({page:page});
+				$route.updateParams({page:page})
 
 			}
 		}])
 })(angular)
-
-/*
-
-	module.controller('InTheatersContorller', ['$scope','$http', function($scope,$http) {
-		//控制器的编写
-		//豆瓣的数据 ajax获取
-
-		ng提供了jsonp的请求方式，奈何豆瓣不支持所以只好自己写jsonp了
-		$scope.subjects = [];
-		var InTheatersAPI = 'http://api.douban.com/v2/movie/in_theaters';
-		$http.jsonp(InTheatersAPI+'?callback:JSON_CALLBACK').then(function( res ){
-			$scope.subjects = res.data.subjects;
-			console.log(res);
-		},function (err) {
-			console.log(err);
-		})
-
-
-		先尝试加载本地的文件
-		$http.get('/app/data1.json').then(function( res ){
-			$scope.subjects = res.data.subjects;
-			console.log(res);
-		},function (err) {
-			console.log(err);
-		})
-	}])
-*/
